cc841c08924d870b5ba6d1e7ced6246c808a27a0
Bernard Krehula <krehula.bernard456@gmail.com>
2025-07-01 20:56:19 +0200

    cart data array

---
+++ my-app/src/cartData.js
@@ -0,0 +1,33 @@
+const cartData = [
+    {
+        id: '1',
+        name: 'Sushi',
+        info: 'Finest fish and veggies',
+        price: '22.99',
+        amount: '1'
+    },
+    {
+        id: '2',
+        name: 'Schnitzel',
+        info: 'A german specialty!',
+        price: '16.5',
+        amount: '1'
+    },
+    {
+        id: '3',
+        name: 'Barbecue Burger',
+        info: 'American, raw, meaty',
+        price: '12.99',
+        amount: '1'
+    },
+    {
+        id: '4',
+        name: 'Green Bowl',
+        info: 'Healthy...and green...',
+        price: '18.99',
+        amount: '1'
+    }
+
+]
+
+export default cartData;
